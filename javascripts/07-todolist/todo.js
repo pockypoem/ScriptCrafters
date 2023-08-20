3d68e76c45dc08f80f85bf424a50691dd64646d3
pockypoem <jeremyas.cornelis@gmail.com>
2023-08-20 16:31:05 +0700

    display todolist

--- javascripts/07-todolist/todo.js
+++ javascripts/07-todolist/todo.js
@@ -1,7 +1,41 @@
-const todolist = [];
+const todolist = [
+    "Belajar Javascript Dasar",
+    "Belajar Javascript Object Oriented Programming",
+    "Belajar Javascript Document Object Model"
+];
 
+function clearTodoList() {
+    const todolistBody = document.getElementById("todolistBody");
+    while(todolistBody.firstChild) {
+        todolistBody.removeChild(todolistBody.firstChild);
+    }
+}
 
+// TODO: Display Todo
+function displayTodoList() {
 
+    clearTodoList();
+
+    for(let i = 0; i < todolist.length; i++) {
+        const todo = todolist[i];
+
+        const tr = document.createElement("tr");
+        const tdButton = document.createElement("td");
+        tr.appendChild(tdButton);
+
+        const buttonDone = document.createElement("input");
+        buttonDone.type = "button";
+        buttonDone.value = "Done";
+        tdButton.appendChild(buttonDone);
+
+        const tdTodo = document.createElement("td");
+        tdTodo.textContent = todo;
+        tr.appendChild(tdTodo);
+
+        const todolistBody = document.getElementById("todolistBody");
+        todolistBody.appendChild(tr);
+    }
+}
 
 
 // TODO: Add Todo
@@ -14,4 +48,7 @@ document.forms['todoForm'].onsubmit = function (event) {
     document.forms['todoForm'].reset();
 
     console.info(todolist);
+    displayTodoList();
 }
+
+displayTodoList();
