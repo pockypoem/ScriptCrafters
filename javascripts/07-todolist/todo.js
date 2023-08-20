21f470f5948599117b91c010e97f0b366a4db4a6
pockypoem <jeremyas.cornelis@gmail.com>
2023-08-20 16:52:08 +0700

    search todolist

--- javascripts/07-todolist/todo.js
+++ javascripts/07-todolist/todo.js
@@ -11,15 +11,8 @@ function clearTodoList() {
     }
 }
 
-// TODO: Display Todo
-function displayTodoList() {
-
-    clearTodoList();
-
-    for(let i = 0; i < todolist.length; i++) {
-        const todo = todolist[i];
-
-        const tr = document.createElement("tr");
+function addTodoList(todo) {
+    const tr = document.createElement("tr");
         const tdButton = document.createElement("td");
         tr.appendChild(tdButton);
 
@@ -34,6 +27,23 @@ function displayTodoList() {
 
         const todolistBody = document.getElementById("todolistBody");
         todolistBody.appendChild(tr);
+}
+
+
+// TODO: Display Todo
+function displayTodoList() {
+
+    clearTodoList();
+
+    for(let i = 0; i < todolist.length; i++) {
+        const todo = todolist[i];
+
+        const searchText = document.getElementById("search").value.toLowerCase();
+
+        if (todo.toLowerCase().includes(searchText)) {
+            addTodoList(todo);
+        }
+
     }
 }
 
@@ -48,6 +58,18 @@ document.forms['todoForm'].onsubmit = function (event) {
     document.forms['todoForm'].reset();
 
     console.info(todolist);
+    displayTodoList();
+}
+
+// TODO: Search Todolist
+
+const searchInput = document.getElementById("search");
+
+searchInput.onkeyup = function() {
+    displayTodoList();
+}
+
+searchInput.onkeydown = function() {
     displayTodoList();
 }
 
