1602c41251fbf5efac61fc38368a7ecee37d2804
pockypoem <jeremyas.cornelis@gmail.com>
2023-08-20 16:06:08 +0700

    add todolist

---
+++ javascripts/07-todolist/todo.js
@@ -0,0 +1,17 @@
+const todolist = [];
+
+
+
+
+
+// TODO: Add Todo
+document.forms['todoForm'].onsubmit = function (event) {
+    event.preventDefault();
+
+    const todo = document.forms['todoForm']['todo'].value;
+    todolist.push(todo);
+
+    document.forms['todoForm'].reset();
+
+    console.info(todolist);
+}
